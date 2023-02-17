2f2da86d499bc59d592dffff567035662a6239cc
diego-cum-telus <79596068+diego-cum-telus@users.noreply.github.com>
2023-02-16 20:05:43 -0600

    feat(node): exercise

--- index.js
+++ index.js
@@ -1,29 +1,62 @@
+/**
+ * FreeCodeCamp Timestamp microservice
+ * Receive a string parameter and gives a JSON with unix and natural date format
+ * @author Lior Chamla
+ */
  var http = require('http');
  var path = require('path');
  var express = require('express');
  var strftime = require('strftime');
- var router = express();
- var server = http.createServer(router);
- 
- router.use(express.static(path.resolve(__dirname, 'client')));
- 
+ var app = express();
+ // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
- router.get('/api/:date', function(req, res){
-   var date = new Date();
-   if(/^\d*$/.test(req.params.date)){
-     date.setTime(req.params.date);
-   } 
-   else {
-     date = new Date(req.params.date);
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+// your first API endpoint... 
+app.get("/api/hello", function (req, res) {
+  res.json({greeting: 'hello API'});
+});
+
+ 
+ // route on GET with a parameter we call :date
+ app.get('/api/:date?', (req, res) => {
+   const dateString = req.params.date;
+   const dateStringRegex = /^[0-9]+$/;
+   const numbersOnly = dateStringRegex.test(dateString);
+ 
+   if (!numbersOnly) {
+     const unixTimestamp = Date.parse(dateString);
+     const utcDate = new Date(unixTimestamp).toUTCString();
+     console.log({ unix: unixTimestamp, utc: utcDate });
+     unixTimestamp
+       ? res.json({ unix: unixTimestamp, utc: utcDate })
+       : res.json({ error: 'Invalid Date' });
+   } else {
+     const unixTimestamp = parseInt(dateString);
+     const actualDate = new Date(unixTimestamp);
+     const utcDate = actualDate.toUTCString();
+     console.log({ unix: unixTimestamp, utc: utcDate });
+     res.json({ unix: unixTimestamp, utc: utcDate });
    }
-   res.set({ 'Content-Type': 'application/json' }) 
-   if(!date.getTime()) res.send(JSON.stringify({error: "Invalid date given"}))
-   else res.send(JSON.stringify({
-     unix: date.getTime(),
-     natural: strftime('%B %d, %Y', date)
-   }))
- })
- server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
-   var addr = server.address();
-   console.log("Timestamp microservice listening at", addr.address + ":" + addr.port);
+ 
+   app.get('/api', (req, res) => {
+     const currentDate = new Date().toUTCString();
+     const currentUnix = Date.parse(currentDate);
+     res.json({ unix: currentUnix, utc: currentDate });
+   });
  });
+ 
+ var listener = app.listen(3000, function () {
+   console.log('Your app is listening on port ' + listener.address().port);
+ });
+ 
