1f7095619ceebcd8e5a833566a28d3cef3f4f36a
diego-cum-telus <79596068+diego-cum-telus@users.noreply.github.com>
2023-02-16 19:22:03 -0600

    feat(node): exercise node

--- index.js
+++ index.js
@@ -1,32 +1,29 @@
-// index.js
-// where your node app starts
+ var http = require('http');
+ var path = require('path');
+ var express = require('express');
+ var strftime = require('strftime');
+ var router = express();
+ var server = http.createServer(router);
+ 
+ router.use(express.static(path.resolve(__dirname, 'client')));
+ 
 
-// init project
-var express = require('express');
-var app = express();
-
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
-
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
-
-// http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
-});
-
-
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
-
-
-
-// listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
-});
+ router.get('/:date', function(req, res){
+   var date = new Date();
+   if(/^\d*$/.test(req.params.date)){
+     date.setTime(req.params.date);
+   } 
+   else {
+     date = new Date(req.params.date);
+   }
+   res.set({ 'Content-Type': 'application/json' }) 
+   if(!date.getTime()) res.send(JSON.stringify({error: "Invalid date given"}))
+   else res.send(JSON.stringify({
+     unix: date.getTime(),
+     natural: strftime('%B %d, %Y', date)
+   }))
+ })
+ server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
+   var addr = server.address();
+   console.log("Timestamp microservice listening at", addr.address + ":" + addr.port);
+ });
