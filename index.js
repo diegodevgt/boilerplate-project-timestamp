2adf90a06c827320b05cfcaecbfe993dcdb64455
Diego Cum <79596068+diego-cum-telus@users.noreply.github.com>
2023-02-17 10:59:05 -0600

    feat(exercise): exercise 02

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
-/**
- * FreeCodeCamp Timestamp microservice
- * Receive a string parameter and gives a JSON with unix and natural date format
- * @author Lior Chamla
- */
- var http = require('http');
- var path = require('path');
- var express = require('express');
- var strftime = require('strftime');
- var app = express();
- // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
+require('dotenv').config();
+// server.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+
+app.get("/api/whoami", function (req, res) {
+  const ip = req.ip;
+  const language = req.headers['accept-language'];
+  const software = req.headers['user-agent'];
+  res.send({ip: ip, language: language, software: software});
+})
+
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({optionSuccessStatus: 200})); 
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -21,43 +24,12 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
-// your first API endpoint... 
+// your first API endpoint...
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-app.get('/api', (req, res) => {
-  const currentDate = new Date().toUTCString();
-  const currentUnix = Date.parse(currentDate);
-  res.json({ unix: currentUnix, utc: currentDate });
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
 });
- 
- // route on GET with a parameter we call :date
- app.get('/api/:date?', (req, res) => {
-   const dateString = req.params.date;
-   const dateStringRegex = /^[0-9]+$/;
-   const numbersOnly = dateStringRegex.test(dateString);
- 
-   if (!numbersOnly) {
-     const unixTimestamp = Date.parse(dateString);
-     const utcDate = new Date(unixTimestamp).toUTCString();
-     console.log({ unix: unixTimestamp, utc: utcDate });
-     unixTimestamp
-       ? res.json({ unix: unixTimestamp, utc: utcDate })
-       : res.json({ error: 'Invalid Date' });
-   } else {
-     const unixTimestamp = parseInt(dateString);
-     const actualDate = new Date(unixTimestamp);
-     const utcDate = actualDate.toUTCString();
-     console.log({ unix: unixTimestamp, utc: utcDate });
-     res.json({ unix: unixTimestamp, utc: utcDate });
-   }
- });
-
-
- 
- var listener = app.listen(3000, function () {
-   console.log('Your app is listening on port ' + listener.address().port);
- });
- 
